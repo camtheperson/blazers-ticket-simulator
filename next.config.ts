76505c64cd8008bbfe6cbf92a519eb432a801c3b
Cameron Hermens <cameron.hermens@onxmaps.com>
2024-10-23 16:32:38 -0700

    Fix assets

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/blazers-ticket-simulator'
+  basePath: '/blazers-ticket-simulator',
+  assetPrefix: '/blazers-ticket-simulator/',
 };
 
 export default nextConfig;
