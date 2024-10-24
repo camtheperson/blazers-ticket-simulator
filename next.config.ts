869757c4029b849fd70e8b1338cbe9a5ac74a8b2
Cameron Hermens <cameron.hermens@onxmaps.com>
2024-10-24 14:08:44 -0700

    Revert config changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const isDev = process?.env?.NODE_ENV !== 'production';
+// const isDev = process?.env?.NODE_ENV !== 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isDev ? '' : '/blazers-ticket-simulator',
-  assetPrefix: isDev ? '' : '/blazers-ticket-simulator/',
+  basePath: 'blazers-ticket-simulator',
+  assetPrefix: '/blazers-ticket-simulator/',
   images: {
     unoptimized: true, 
   }
